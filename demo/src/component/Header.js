c4258462bae1473639c4d7a25d7a4daf0260573b
hao2dev <nghuuhao182001@gmail.com>
2022-11-22 01:37:04 +0700

    seccond commit

--- demo/src/component/Header.js
+++ demo/src/component/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-        <h1 className="text-center">PROJECT</h1>
+        <h1 className="text-center">PROJECT First</h1>
     )
   }
 }
